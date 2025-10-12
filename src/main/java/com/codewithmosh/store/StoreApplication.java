bce3d9273aa4ad47fbab28f2df40d5d7852878f8
aghiles-abib <aghiles.abib@gmail.com>
2025-10-12 22:55:03 +0200

    feat: clean code and add entities

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.store;
 
+import com.codewithmosh.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +11,11 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+         var user =User.builder().name("aghiles")
+                 .password("password")
+                 .email("email")
+                 .build();
+
     }
 
 }
