6cf3407099182086cdedb184890ebf898625bd07
aghiles-abib <aghiles.abib@gmail.com>
2025-10-14 23:16:46 +0200

    Add many to many relation

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,21 +1,21 @@
 package com.codewithmosh.store;
 
 import com.codewithmosh.store.entities.Address;
+import com.codewithmosh.store.entities.Tag;
 import com.codewithmosh.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-         var user =User.builder().name("aghiles")
-                 .password("password")
-                 .email("email")
-                 .build();
+        var user = User.builder().name("aghiles")
+                .password("password")
+                .email("email")
+                .build();
 
         var address = Address.builder()
                 .street("aghiles")
@@ -25,6 +25,10 @@ public class StoreApplication {
                 .build();
 
         user.addAddress(address);
+        System.out.println(user);
+
+        user.addTag("tag1");
+
         System.out.println(user);
 
 
--- src/main/java/com/codewithmosh/store/entities/Tag.java
+++ src/main/java/com/codewithmosh/store/entities/Tag.java
@@ -3,11 +3,13 @@ package com.codewithmosh.store.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@ToString
 @Entity
 @Table(name="tags")
 public class Tag {
@@ -19,4 +21,11 @@ public class Tag {
     @Column(name = "name")
     private String name;
 
+    @ManyToMany(mappedBy = "tags")
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
