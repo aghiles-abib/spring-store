afdc5f3bbf5f83b289d1b554ff15e71a4ea2e25c
aghiles-abib <aghiles.abib@gmail.com>
2025-10-19 22:59:00 +0200

    feat: add repositories

--- src/main/java/com/codewithmosh/store/StoreApplication.java
+++ src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -4,6 +4,7 @@ import com.codewithmosh.store.entities.Address;
 import com.codewithmosh.store.entities.Profile;
 import com.codewithmosh.store.entities.Tag;
 import com.codewithmosh.store.entities.User;
+import com.codewithmosh.store.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,31 +14,37 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var user = User.builder().name("aghiles")
-                .password("password")
-                .email("email")
-                .build();
 
-        var address = Address.builder()
-                .street("aghiles")
-                .city("password")
-                .state("state")
-                .zip("zip")
-                .build();
+        var repository = context.getBean(UserRepository.class);
 
-        user.addAddress(address);
-        System.out.println(user);
+        repository.findAll().forEach(u -> System.out.println(u.getEmail()));
 
-        user.addTag("tag1");
 
-        var profile = Profile.builder()
-                        .bio("bio")
-                                .build();
-        user.setProfile(profile);
-
-        profile.setUser(user);
-
-        System.out.println(user);
+//        var user = User.builder().name("aghiles")
+//                .password("password")
+//                .email("email")
+//                .build();
+//        repository.save(user);
+//        var address = Address.builder()
+//                .street("aghiles")
+//                .city("password")
+//                .state("state")
+//                .zip("zip")
+//                .build();
+//
+//        user.addAddress(address);
+//        System.out.println(user);
+//
+//        user.addTag("tag1");
+//
+//        var profile = Profile.builder()
+//                        .bio("bio")
+//                                .build();
+//        user.setProfile(profile);
+//
+//        profile.setUser(user);
+//
+//        System.out.println(user);
 
 
     }
